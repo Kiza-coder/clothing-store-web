321a6d7d1529741438dbcef1cca480f7a5df3106
Kiza-coder <kiza.motoko@gmail.com>
2024-04-12 00:55:02 +0200

    Update Authentidication

--- src/components/routes/sign-in/sign-in.component.js
+++ src/components/routes/sign-in/sign-in.component.js
@@ -1,19 +1,35 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../../utils/firebase/firebase.util";
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+import {
+    auth,
+    signInWithGooglePopup,
+    signInWithGoogleRedirect,
+    createUserDocumentFromAuth
+} from "../../../utils/firebase/firebase.util";
 
 const SignIn = () => {
 
+
+
+
+
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
+
         const userDocRef = await createUserDocumentFromAuth(user);
     }
 
-    return(
+
+
+    return (
         <div>
             <h1>SignIn</h1>
             <button onClick={logGoogleUser}>
                 Sign In With Google Pop UP
             </button>
+
         </div>
     )
 }
-export default  SignIn;
+
+export default SignIn;
