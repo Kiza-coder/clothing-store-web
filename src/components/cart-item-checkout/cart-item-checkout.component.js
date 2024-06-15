d85d20ab9729ddcbf0ce8b0f6e7f756379a8610c
Kiza-coder <kiza.motoko@gmail.com>
2024-06-15 13:59:40 +0200

    Add styles css

--- src/components/cart-item-checkout/cart-item-checkout.component.js
+++ src/components/cart-item-checkout/cart-item-checkout.component.js
@@ -1,27 +1,43 @@
 import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart.context';
+import './cart-item-checkout.styles.css';
+
 
 const CartItemCheckout = ({ cartItem }) => {
 
-const { addItemToCart, removeItemToCart, removeItemFromCheckout } = useContext(CartContext);
+  const { addItemToCart, removeItemToCart, removeItemFromCheckout } = useContext(CartContext);
 
-const addProductToCart = () =>  addItemToCart(cartItem);
-const removeProductToCart = () =>  removeItemToCart(cartItem);
-const removeProductFromCheckout = () => removeItemFromCheckout(cartItem);
+  const addProductToCart = () => addItemToCart(cartItem);
+  const removeProductToCart = () => removeItemToCart(cartItem);
+  const removeProductFromCheckout = () => removeItemFromCheckout(cartItem);
 
 
   const { imageUrl, name, quantity, price } = cartItem;
   return (
-    <div>
-      <div>{imageUrl}</div>
-      <div>{name}</div>
-      <div>
-        <div onClick={addProductToCart}> + </div>
-        {quantity}
-        <div onClick={removeProductToCart}> - </div>
+    <div className='checkout-item-container'>
+
+      <div className='image-container'>
+        <img src={imageUrl} alt={`${name}`} />
       </div>
-      <div>{price}</div>
-      <div onClick={removeProductFromCheckout}>remove</div>
+
+      <span className='name'>{name}</span>
+
+      <span className="quantity">
+        <div className="arrow" onClick={ removeProductToCart}>
+        &#10094;
+        </div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={addProductToCart}>
+        &#10095;
+        </div>
+      </span>
+
+      <span className="price">{price}</span>
+
+      <div className="remove-button" onClick={removeProductFromCheckout}>
+      &#10005;
+      </div>
+
     </div>
   );
 };
--- src/routes/checkout/checkout.component.js
+++ src/routes/checkout/checkout.component.js
@@ -1,31 +1,49 @@
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart.context';
 import CartItemCheckout from '../../components/cart-item-checkout/cart-item-checkout.component';
+import './checkout.styles.css';
+
 
 
 
 const Checkout = () => {
 
-    const { cartItems, totalPrice } = useContext(CartContext);
+  const { cartItems, totalPrice } = useContext(CartContext);
 
-  
-  
-    
-    return(
-        <div className="checkout-container">
-           <div>Titles</div>
-           <div>
-           {cartItems.length ? (
+
+
+
+  return (
+    <div className="checkout-container">
+      <div className="checkout-header">
+        <div className='header-block'>
+          <span>Product</span>
+        </div>
+        <div className='header-block'>
+          <span>Description</span>
+        </div>
+        <div className='header-block'>
+          <span>Quantity</span>
+        </div>
+        <div className='header-block'>
+          <span>Price</span>
+        </div>
+        <div className='header-block'>
+          <span>Remove</span>
+        </div>
+      </div>
+      <div>
+        {cartItems.length ? (
           cartItems.map((item) => <CartItemCheckout key={item.id} cartItem={item} />)
         ) : (
           <span className="empty-message">Your cart is empty</span>
         )}
-           </div>
-           <div>
-            Total : {totalPrice} $
-           </div>
-        </div>
-    );
-}
+      </div>
+      <div className='total'>
+        Total : {totalPrice} $
+      </div>
+    </div>
+  );
+};
 
 export default Checkout;
